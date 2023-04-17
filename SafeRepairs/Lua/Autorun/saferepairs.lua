2ea3787786eb11c4a469763b8a597f26187985c3
xolophreny <xolophreny@proton.me>
2023-04-18 03:41:44 +0500

    SafeRepairs: minor progress, not working yet

--- SafeRepairs/Lua/Autorun/saferepairs.lua
+++ SafeRepairs/Lua/Autorun/saferepairs.lua
@@ -15,37 +15,47 @@ SR.PowerTransferItemStates = {}
 	end
 end) --]]
 
-Hook.Patch("Barotrauma.Items.Components.Repairable", "StartRepair", function (instance, ptable)
+Hook.Patch("Barotrauma.Items.Components.Repairable", "StartRepairing", function (instance, ptable)
 	local powered = instance.Item.GetComponentString("Powered")
 	local powertransfer = instance.Item.GetComponentString("PowerTransfer")
 	local id = instance.Item.ID
 
 	if ptable.ReturnValue == true then
 		print("Safely repairing ", id)
-		if powered then
-			table.insert(SR.PoweredItemStates, id, powered.IsActive())
+		if powered ~= nil then
+			if SR.PoweredItemStates[id] == nil then
+				SR.PoweredItemStates[id] = powered.IsActive
+			end
+			print("Powered state is ", powered.IsActive)
 			powered.IsActive = false
 		end
-		if powertransfer then
-			table.insert(SR.PowerTransferItemStates, id, powertransfer.IsActive())
+		if powertransfer ~= nil then
+			if SR.PowerTransferItemStates[id] == nil then
+				SR.PowerTransferItemStates[id] = powertransfer.IsActive
+			end
+			print("PowerTransfer state is ", powertransfer.IsActive)
 			powertransfer.IsActive = false
 		end
 	end
 
 end, Hook.HookMethodType.After)
 
-Hook.Patch("Barotrauma.Items.Components.Repairable", "StopRepair", function (instance, ptable)
+Hook.Patch("Barotrauma.Items.Components.Repairable", "StopRepairing", function (instance, ptable)
 	local powered = instance.Item.GetComponentString("Powered")
 	local powertransfer = instance.Item.GetComponentString("PowerTransfer")
 	local id = instance.Item.ID
 
 	if ptable.ReturnValue == true then
 		print("Done safely repairing ", id)
-		if powered then
+		if powered ~= nil then
+			print("Powered state was ", SR.PoweredItemStates[id])
 			powered.IsActive = SR.PoweredItemStates[id]
+			SR.PoweredItemStates[id] = nil
 		end
-		if powertransfer then
+		if powertransfer ~= nil then
+			print("PowerTransfer state was ", SR.PowerTransferItemStates[id])
 			powertransfer.IsActive = SR.PowerTransferItemStates[id]
+			SR.PowerTransferItemStates[id] = nil
 		end
 	end
 
